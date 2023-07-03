650b31b7e904829daaa07e94493f0854071d452d
n.rasmussen <nodrasmu@uwaterloo.ca>
2023-07-03 12:18:37 -0400

    July 3nd version

--- Characters.py
+++ Characters.py
@@ -23,12 +23,12 @@ class Player(pg.sprite.Sprite):
         # self.rect = self.surf.get_rect()
         self.SCREEN_WIDTH = SCREEN_WIDTH
         self.SCREEN_HEIGHT = SCREEN_HEIGHT
-        self.surf = pg.image.load("car.png").convert()
-        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.surf = pg.transform.scale(self.surf, (75, 25))
-        self.rect = self.surf.get_rect(center=(0, SCREEN_HEIGHT/2))
+        self.surf = pg.image.load("car.png").convert()  # loads the image of the racecar
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)  # set the white colour to transparent (background of car)
+        self.surf = pg.transform.scale(self.surf, (75, 25))  # scales the image
+        self.rect = self.surf.get_rect(center=(0, SCREEN_HEIGHT/2))  # draws the rectangle for the shape at location
 
-    def update_player(self, pressed_keys):
+    def update_player(self, pressed_keys):  # Method to move the player with arrow key press
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -1)
         if pressed_keys[K_DOWN]:
@@ -38,7 +38,7 @@ class Player(pg.sprite.Sprite):
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(1, 0)
 
-        if self.rect.left < 0:
+        if self.rect.left < 0:  # conditions to keep player within the viewable screen
             self.rect.left = 0
         if self.rect.right > self.SCREEN_WIDTH:
             self.rect.right = self.SCREEN_WIDTH
@@ -48,24 +48,24 @@ class Player(pg.sprite.Sprite):
             self.rect.bottom = self.SCREEN_HEIGHT
 
 
-class Enemy(pg.sprite.Sprite):
-    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT, t):
+class Enemy(pg.sprite.Sprite):  # Class to create an enemy object
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT, t):  # Method to generate enemy
         super().__init__()
-        self.surf = pg.image.load("cone.png").convert()
-        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.surf = pg.transform.scale(self.surf, (75, 75))
-        self.rect = self.surf.get_rect(
+        self.surf = pg.image.load("cone.png").convert()  # loads the image of the traffic cone
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)  # makes the white of the image transparent
+        self.surf = pg.transform.scale(self.surf, (75, 75))   # scales the image
+        self.rect = self.surf.get_rect(  # randomly generates the object and places it off the screen to the right
             center=(
                 random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
                 random.randint(0, SCREEN_HEIGHT)
             )
         )
-        self.speed = random.randint(1, 3) * (t / 20 + 0.25)
+        self.speed = random.randint(1, 5) * (t / 100 + 0.25)  # sets the speed at which the object will move
 
-    def update(self):
+    def update(self):  # method to move the enemy to the left at specified speed
         self.rect.move_ip(-self.speed, 0)
         if self.rect.right < 0:
-            self.kill()
+            self.kill()  # Kills the enemy once it moves off the screen
 
 
 
--- test.py
+++ test.py
@@ -15,54 +15,54 @@ from pygame.locals import (
     QUIT,
 )
 
-class Stopwatch():
+
+class Stopwatch:  # this class creates a timer to control the speed of enemies
     def __int__(self, start):
         self._start = start
 
-    def get_seconds(self):
+    def get_seconds(self): # this method tracks how long the game has been going on for
         return time.time() - self._start
 
 
-def run_game():  # main Game Loop this check if the game has been closed
-    pg.init()
-    t = Stopwatch()
-    t._start = time.time()
-    SCREEN_WIDTH = 1200
-    SCREEN_HEIGHT = 800
-    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pg.display.set_caption("Basic Game!")
+def run_game():  # main Game Loop
+    pg.init()  # initializes imported pygame modules
+    t = Stopwatch()  # create game timer object
+    t._start = time.time()  # sets the start time on the game timer
+    SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 800  # sets screen dimensions
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # makes screen
+    pg.display.set_caption("Basic Game!") # makes window name
     # Creates "Player"
-    player = Characters.Player(SCREEN_WIDTH, SCREEN_HEIGHT)
-    enemies = pg.sprite.Group()
-    all_sprites = pg.sprite.Group()
-    all_sprites.add(player)
+    player = Characters.Player(SCREEN_WIDTH, SCREEN_HEIGHT)  # creates player object
+    enemies = pg.sprite.Group()  # makes a bucket to hold enemies
+    all_sprites = pg.sprite.Group()  # makes a bucket to hold enemies and player
+    all_sprites.add(player)  # adds player to bucket holding all Sprites
 
-    counter, text = 0, '0'
-    font = pg.font.SysFont('Consolas', 30)
+    counter, text = 0, '0'  # initalize the timer in game
+    font = pg.font.SysFont('Consolas', 30)  # sets fond and size of in game timer
 
-    ADDENEMY = pg.USEREVENT + 1
-    pg.time.set_timer(ADDENEMY, 200)
-    timer = pg.USEREVENT + 2
-    pg.time.set_timer(timer, 1000)
+    ADDENEMY = pg.USEREVENT + 1  # Create an event called ADDENEMY and asigns it to the next available event int
+    pg.time.set_timer(ADDENEMY, 200)  # places the event (ADDENEMY) in the event queue every 200 ms
+    timer = pg.USEREVENT + 2   # Create an event called timer and asigns it to the next available event int
+    pg.time.set_timer(timer, 1000)  # places the event (timer) in the event queue every 1 s
 
     while True:
-        for event in pg.event.get(): # looks for event to close game
-            if event.type == KEYDOWN:
+        for event in pg.event.get():  # iterate through the event queue
+            if event.type == KEYDOWN:  # if event is escape quite program
                 if event.key == K_ESCAPE:
                     sys.exit()
-            elif event.type == QUIT:
+            elif event.type == QUIT:  # if event is quit window quit program
                 sys.exit()
 
-            elif event.type == ADDENEMY:
+            elif event.type == ADDENEMY:  # if event is ADDENEMY generate a new enemy
                 new_enemy = Characters.Enemy(SCREEN_WIDTH, SCREEN_HEIGHT, t.get_seconds())
                 enemies.add(new_enemy)
                 all_sprites.add(new_enemy)
 
-            if event.type == timer:
+            if event.type == timer:  # if event is timer, add 1s to the displayed timer
                 counter += 1
                 text = str(counter)
 
-        pressed_keys = pg.key.get_pressed()
+        pressed_keys = pg.key.get_pressed()  #
         player.update_player(pressed_keys)
 
         enemies.update()
